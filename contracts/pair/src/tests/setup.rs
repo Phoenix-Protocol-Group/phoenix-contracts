5bd78b569f1ad660083f2645c42cc92aad55eaa0
Totally not Jakub <totallynotjakub@proton.me>
2023-06-20 01:08:15 +0200

    Pair: swap implementation tests

---
+++ contracts/pair/src/tests/setup.rs
@@ -0,0 +1,31 @@
+use soroban_sdk::{Address, BytesN, Env};
+
+use crate::{
+    contract::{LiquidityPool, LiquidityPoolClient},
+    token_contract,
+};
+
+pub fn deploy_token_contract<'a>(env: &Env, admin: &Address) -> token_contract::Client<'a> {
+    token_contract::Client::new(env, &env.register_stellar_asset_contract(admin.clone()))
+}
+
+fn install_token_wasm(env: &Env) -> BytesN<32> {
+    soroban_sdk::contractimport!(
+        file = "../token/target/wasm32-unknown-unknown/release/soroban_token_contract.wasm"
+    );
+    env.install_contract_wasm(WASM)
+}
+
+pub fn deploy_liquidity_pool_contract<'a>(
+    env: &Env,
+    token_a: &Address,
+    token_b: &Address,
+) -> LiquidityPoolClient<'a> {
+    let pool = LiquidityPoolClient::new(env, &env.register_contract(None, LiquidityPool {}));
+    let token_wasm_hash = install_token_wasm(env);
+    let share_token_decimals = 7u32;
+    pool.initialize(&token_wasm_hash, token_a, token_b, &share_token_decimals);
+    pool
+}
+
+
--- contracts/pair/src/tests/swap.rs
+++ contracts/pair/src/tests/swap.rs
@@ -7,7 +7,7 @@ use crate::{
 };
 
 #[test]
-fn provide_liqudity() {
+fn simple_swap() {
     let env = Env::default();
     env.mock_all_auths();
 
@@ -26,40 +26,33 @@ fn provide_liqudity() {
     let token_share = token_contract::Client::new(&env, &pool.query_share_token_address());
 
     token1.mint(&user1, &1000);
-    assert_eq!(token1.balance(&user1), 1000);
-
     token2.mint(&user1, &1000);
-    assert_eq!(token2.balance(&user1), 1000);
-
     pool.provide_liquidity(&user1, &100, &100, &100, &100);
+
+    // true means "selling A token"
+    // selling just one token with 10% max spread allowed
+    pool.swap(&user1, &true, &1, &10);
     assert_eq!(
         env.auths(),
         [
             (
                 user1.clone(),
                 pool.address.clone(),
-                Symbol::new(&env, "provide_liquidity"),
-                (&user1, 100_u128, 100_u128, 100_u128, 100_u128).into_val(&env)
+                Symbol::short("swap"),
+                (&user1, true, 1_i128, 100_i128).into_val(&env)
             ),
             (
                 user1.clone(),
                 token1.address.clone(),
                 Symbol::short("transfer"),
-                (&user1, &pool.address, 100_i128).into_val(&env)
-            ),
-            (
-                user1.clone(),
-                token2.address.clone(),
-                Symbol::short("transfer"),
-                (&user1, &pool.address, 100_i128).into_val(&env)
-            ),
+                (&user1, &pool.address, 1_i128).into_val(&env)
+            )
         ]
     );
 
-    assert_eq!(token_share.balance(&user1), 100);
-    assert_eq!(token_share.balance(&pool.address), 0);
-    assert_eq!(token1.balance(&user1), 900);
-    assert_eq!(token1.balance(&pool.address), 100);
-    assert_eq!(token2.balance(&user1), 900);
-    assert_eq!(token2.balance(&pool.address), 100);
+    assert_eq!(token1.balance(&user1), 899);
+    assert_eq!(token1.balance(&pool.address), 101);
+    assert_eq!(token2.balance(&user1), 1001);
+    assert_eq!(token2.balance(&pool.address), 99);
+
 }
