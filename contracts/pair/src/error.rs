db39bcacb5851be1e70a906aa06304074d12deab
Kaloyan Gangov <gangov1@gmail.com>
2023-08-15 19:00:20 +0300

    Pair: refactors the values of ContractError enum

--- contracts/pair/src/error.rs
+++ contracts/pair/src/error.rs
@@ -3,26 +3,28 @@ use soroban_sdk::contracterror;
 #[contracterror]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
-pub enum SwapError {
-    SlippageToleranceExceeded = 1,
-    SlippageToleranceViolated = 2,
-    SpreadExceedsMaxAllowed = 3,
-}
-
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
 pub enum ContractError {
+    /// Swap errors
+    SlippageToleranceExceeded = 2,
+    SlippageToleranceViolated = 3,
+    SpreadExceedsMaxAllowed = 4,
+    EmptyPoolBalance = 11,
+
+    /// Initialization errors
     FirstTokenMustBeSmallerThenSecond = 1,
-    Swapage(SwapError) = 2,
-    ConfigNotSet = 3,
-    FailedToLoadFromStorage = 4,
-    IncorrectLiqudityParameters = 5,
-    DepositAmountExceedsOrBelowMin = 6,
-    WithdrawMinNotSatisfied = 7,
-    InvalidFeeBps = 8,
-    EmptyPoolBalance = 9,
-    InvalidAmounts = 10,
-    Unauthorized = 11,
-    ArgumentsInvalidLessOrEqualZero = 12,
+    InvalidFeeBps = 10,
+
+    // Storage errors
+    ConfigNotSet = 5,
+    FailedToLoadFromStorage = 6,
+    IncorrectLiqudityParameters = 7,
+    DepositAmountExceedsOrBelowMin = 8,
+
+    /// Liquidity errors
+    WithdrawMinNotSatisfied = 9,
+    InvalidAmounts = 12,
+
+    /// Other errors
+    Unauthorized = 13,
+    ArgumentsInvalidLessOrEqualZero = 14,
 }
