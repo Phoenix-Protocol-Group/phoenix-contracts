cae2356943c7b4d814b3579786e4c5b9711d3e28
Kaloyan Gangov <gangov1@gmail.com>
2023-08-25 19:27:36 +0300

    Factory: implementing lp contract init

--- contracts/factory/src/contract.rs
+++ contracts/factory/src/contract.rs
@@ -1,7 +1,10 @@
 use soroban_sdk::{contract, contractimpl, contractmeta, log, Address, Env, Vec};
 
-use crate::error::ContractError;
-use crate::storage::{utils, LiquidityPoolInitInfo, StakeInitInfo, TokenInitInfo};
+use crate::{
+    error::ContractError,
+    lp_contract,
+    storage::{utils, LiquidityPoolInitInfo, StakeInitInfo, TokenInitInfo},
+};
 
 // Metadata that is added on to the WASM custom section
 contractmeta!(key = "Description", val = "Phoenix Protocol Factory");
@@ -34,13 +37,33 @@ impl FactoryTrait for Factory {
 
     fn create_liquidity_pool(
         env: Env,
-        _lp_init_info: LiquidityPoolInitInfo,
+        lp_init_info: LiquidityPoolInitInfo,
         token_init_info: TokenInitInfo,
-        _stake_init_info: StakeInitInfo,
+        stake_init_info: StakeInitInfo,
     ) -> Result<(), ContractError> {
         validate_token_info(&env, &token_init_info)?;
 
-        // let lp_contract_address =
+        let lp_contract_address = utils::deploy_lp_contract(
+            &env,
+            lp_init_info.lp_wasm_hash,
+            lp_init_info.share_token_decimals,
+            lp_init_info.swap_fee_bps,
+            lp_init_info.fee_recipient,
+            lp_init_info.max_allowed_slippage_bps,
+            lp_init_info.max_allowed_spread_bps,
+        );
+
+        lp_contract::Client::new(&env, &lp_contract_address).initialize(
+            &utils::get_admin(&env)?,
+            &lp_init_info.share_token_decimals,
+            &lp_init_info.swap_fee_bps,
+            &lp_init_info.fee_recipient,
+            &lp_init_info.max_allowed_slippage_bps,
+            &lp_init_info.max_allowed_spread_bps,
+            // pick it up from here
+            &token_init_info,
+            &stake_init_info,
+        );
         // init liquidity_pool with lp specific info
         // pass the token and stake contract info into it
         // let the underlying actions do the work
