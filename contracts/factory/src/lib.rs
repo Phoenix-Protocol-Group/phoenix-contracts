ca4aecf8b73f4efeabf8df6d2eb80e1d9a5baf3d
Kaloyan Gangov <gangov1@gmail.com>
2023-08-25 16:32:46 +0300

    Factory: resolves PR comments

--- contracts/factory/src/lib.rs
+++ contracts/factory/src/lib.rs
@@ -5,16 +5,6 @@ mod storage;
 
 mod utils;
 
-// lp contract will init token_contract, do we need this one?
-pub mod token_contract {
-    // The import will code generate:
-    // - A ContractClient type that can be used to invoke functions on the contract.
-    // - Any types in the contract that were annotated with #[contracttype].
-    soroban_sdk::contractimport!(
-        file = "../../target/wasm32-unknown-unknown/release/soroban_token_contract.wasm"
-    );
-}
-
 pub mod lp_contract {
     soroban_sdk::contractimport!(
         file = "../../target/wasm32-unknown-unknown/release/phoenix_pair.wasm"
