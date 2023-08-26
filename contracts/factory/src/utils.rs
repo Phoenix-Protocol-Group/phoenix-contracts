a34a4dff539d126f4ef3d31b7bb836d69b9d2776
Kaloyan Gangov <gangov1@gmail.com>
2023-08-26 22:41:59 +0300

    Factory: fixes an error in the token deployment

---
+++ contracts/factory/src/utils.rs
@@ -0,0 +1,19 @@
+use soroban_sdk::{Address, Bytes, BytesN, Env};
+
+pub fn deploy_lp_contract(
+    env: &Env,
+    lp_wasm_hash: BytesN<32>,
+) -> Address {
+    let deployer = env.current_contract_address();
+
+    if deployer != env.current_contract_address() {
+        deployer.require_auth();
+    }
+
+    let salt = Bytes::new(env);
+    let salt = env.crypto().sha256(&salt);
+
+    env.deployer()
+        .with_address(deployer, salt)
+        .deploy(lp_wasm_hash)
+}
