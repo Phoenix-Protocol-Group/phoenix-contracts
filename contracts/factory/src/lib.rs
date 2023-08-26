e7fa2f965a4b3401c9e0829b212ea56903005a75
Kaloyan Gangov <gangov1@gmail.com>
2023-08-26 22:10:37 +0300

    Factory: removes unused structs

--- contracts/factory/src/lib.rs
+++ contracts/factory/src/lib.rs
@@ -4,9 +4,3 @@ mod error;
 mod storage;
 
 mod utils;
-
-pub mod lp_contract {
-    soroban_sdk::contractimport!(
-        file = "../../target/wasm32-unknown-unknown/release/phoenix_pair.wasm"
-    );
-}
