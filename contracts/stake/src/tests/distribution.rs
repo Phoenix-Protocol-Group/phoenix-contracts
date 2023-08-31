112a2f9574eae027fe4d5417b60299557aae852f
Jakub <jakub@moonbite.space>
2023-08-31 15:53:15 +0200

    Stake: Apply points correction when new user bonds tokens

--- contracts/stake/src/tests/distribution.rs
+++ contracts/stake/src/tests/distribution.rs
@@ -305,6 +305,102 @@ fn four_users_with_different_stakes() {
 }
 
 #[test]
+fn two_users_one_starts_after_distribution_begins() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let admin = Address::random(&env);
+    let user = Address::random(&env);
+    let user2 = Address::random(&env);
+
+    let lp_token = deploy_token_contract(&env, &admin);
+    let reward_token = deploy_token_contract(&env, &admin);
+
+    let staking = deploy_staking_contract(&env, admin.clone(), &lp_token.address);
+
+    staking.create_distribution_flow(&admin, &admin, &reward_token.address);
+
+    let reward_amount: u128 = 100_000;
+    reward_token.mint(&admin, &(reward_amount as i128));
+
+    // bond tokens for users; each user has a different amount staked
+    lp_token.mint(&user, &1000);
+    staking.bond(&user, &1000);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = 2_000;
+    });
+
+    let reward_duration = 600;
+    staking.fund_distribution(
+        &admin,
+        &2_000,
+        &reward_duration,
+        &reward_token.address,
+        &(reward_amount as i128),
+    );
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = 2_300;
+    });
+    staking.distribute_rewards();
+
+    // at this points, since half of the time has passed and only one user is staking, he should have 50% of the rewards
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 50_000
+                }
+            ]
+        }
+    );
+
+    // user2 starts staking after the distribution has begun
+    lp_token.mint(&user2, &1000);
+    staking.bond(&user2, &1000);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = 2_600;
+    });
+    staking.distribute_rewards();
+
+    // first user should get 75_000, second user 25_000 since he joined at the half time
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 75_000
+                }
+            ]
+        }
+    );
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user2),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 25_000
+                }
+            ]
+        }
+    );
+
+    staking.withdraw_rewards(&user);
+    assert_eq!(reward_token.balance(&user), 75_000);
+    staking.withdraw_rewards(&user2);
+    assert_eq!(reward_token.balance(&user2), 25_000);
+}
+
+#[test]
 fn fund_rewards_without_establishing_distribution() {
     let env = Env::default();
     env.mock_all_auths();
