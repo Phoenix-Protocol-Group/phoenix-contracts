f8ca525f46f244102a2fad8d3c5829e0d9ef80ad
Kaloyan Gangov <gangov1@gmail.com>
2023-10-02 22:07:56 +0300

    Factory: fixes the auth issues

--- contracts/multihop/src/tests/swap.rs
+++ contracts/multihop/src/tests/swap.rs
@@ -2,36 +2,36 @@ use crate::error::ContractError;
 use crate::tests::setup::{deploy_multihop_contract, deploy_factory_contract, factory};
 use soroban_sdk::arbitrary::std::dbg;
 use soroban_sdk::{testutils::Address as _, vec, Address, Bytes, BytesN, Env};
+use crate::storage::Swap;
 
 #[test]
 fn test_swap() {
     let env = Env::default();
     let admin = Address::random(&env);
 
-    // 1. deploy factory
-    // this fails with all the below given client initializations of factory
-    // either HostError: Error(Value, InvalidInput) or HostError: Error(Context, MissingValue)
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
 
+    // 1. deploy factory
     let factory_addr = deploy_factory_contract(&env, admin.clone());
     let factory_client = factory::Client::new(&env, &factory_addr);
+
     factory_client.initialize(&admin.clone());
 
     let admin = factory_client.get_admin();
-
-    //
-    // dbg!(admin.clone());
+    dbg!(admin.clone());
     // 2. create liquidity pool from factory
     // 3. use the swap method of multihop
     // 4. check if it goes according to plan
 
     // let multihop = deploy_multihop_contract(&env, admin, factory_client.address);
-    //
+    // //
     // let recipient = Address::random(&env);
+    //
     // let swap1 = Swap {
     //     ask_asset: Address::random(&env),
     //     offer_asset: Address::random(&env),
     // };
-    //
     // let swap2 = Swap {
     //     ask_asset: Address::random(&env),
     //     offer_asset: Address::random(&env),
@@ -42,8 +42,7 @@ fn test_swap() {
     // };
     //
     // let swap_vec = vec![&env, swap1, swap2, swap3];
-
-    // WHY WOULD &swap_vec BE MARKED BY THE COMPILER LIKE THAT...
+    // // WHY WOULD &swap_vec BE MARKED BY THE COMPILER LIKE THAT...
     // multihop.swap(&recipient, &swap_vec, &5i128);
 }
 
