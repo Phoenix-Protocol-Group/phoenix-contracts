1412519fc9e5d9b3fd780a1f16321fa160d7ed85
Jakub <jakub@moonbite.space>
2023-10-06 17:15:58 +0200

    Multihop: Refactor algorithm so that user performs its swaps directly

--- contracts/multihop/src/contract.rs
+++ contracts/multihop/src/contract.rs
@@ -1,8 +1,8 @@
-use soroban_sdk::{contract, contractimpl, contractmeta, Address, Env, IntoVal, Symbol, Val, Vec};
+use soroban_sdk::{contract, contractimpl, contractmeta, Address, Env, Vec};
 
 use crate::error::ContractError;
 use crate::storage::{get_factory, save_admin, save_factory, Swap};
-use crate::{factory_contract, lp_contract, token_contract};
+use crate::{factory_contract, lp_contract};
 
 // Metadata that is added on to the WASM custom section
 contractmeta!(
@@ -49,12 +49,10 @@ impl MultihopTrait for Multihop {
 
         recipient.require_auth();
 
-        let mut offer_amount: i128 = amount;
+        // first offer amount is an input from the user,
+        // subsequent are the results of the previous swap
+        let mut next_offer_amount: i128 = amount;
         let mut offer_token_addr: Address = operations.get(0).unwrap().offer_asset.clone();
-        let mut offer_token_client = token_contract::Client::new(&env, &offer_token_addr);
-
-        // first transfer token to multihop contract
-        offer_token_client.transfer(&recipient, &env.current_contract_address(), &offer_amount);
 
         let factory_client =
             factory_contract::Client::new(&env, &get_factory(&env).expect("factory not found"));
@@ -64,27 +62,16 @@ impl MultihopTrait for Multihop {
                 .query_for_pool_by_pair_tuple(&(op.offer_asset, op.ask_asset.clone()));
 
             let lp_client = lp_contract::Client::new(&env, &liquidity_pool_addr);
-            lp_client.swap(
-                &env.current_contract_address(),
+            next_offer_amount = lp_client.swap(
+                &recipient,
                 &true,
-                &offer_amount,
+                &next_offer_amount,
                 &None::<i64>,
                 &Some(5000i64),
             );
 
-            offer_token_client = token_contract::Client::new(&env, &op.ask_asset);
-            offer_amount = offer_token_client.balance(&env.current_contract_address());
             offer_token_addr = op.ask_asset.clone();
         });
-
-        // in each loop iteration, last asked token becomes an offer; after loop we can rename it
-        let asked_amount = offer_amount;
-        let asked_token_addr = offer_token_addr;
-
-        let token_func_name = &Symbol::new(&env, "transfer");
-        let token_call_args: Vec<Val> =
-            (env.current_contract_address(), recipient, asked_amount).into_val(&env);
-        env.invoke_contract::<Val>(&asked_token_addr, token_func_name, token_call_args);
 
         Ok(())
     }
--- contracts/multihop/src/lib.rs
+++ contracts/multihop/src/lib.rs
@@ -5,15 +5,6 @@ mod error;
 
 mod storage;
 
-pub mod token_contract {
-    // The import will code generate:
-    // - A ContractClient type that can be used to invoke functions on the contract.
-    // - Any types in the contract that were annotated with #[contracttype].
-    soroban_sdk::contractimport!(
-        file = "../../target/wasm32-unknown-unknown/release/soroban_token_contract.wasm"
-    );
-}
-
 #[allow(clippy::too_many_arguments)]
 pub mod lp_contract {
     // The import will code generate:
