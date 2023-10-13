1b083b6f8f4e22b644234079f864d9d920897f43
Kaloyan Gangov <gangov1@gmail.com>
2023-10-13 17:05:40 +0300

    Multihop: uncomments the last test

--- contracts/multihop/src/tests/swap.rs
+++ contracts/multihop/src/tests/swap.rs
@@ -8,6 +8,7 @@ use crate::tests::setup::{
 
 use soroban_sdk::arbitrary::std;
 use soroban_sdk::{testutils::Address as _, vec, Address, Env};
+use soroban_sdk::arbitrary::std::dbg;
 
 #[test]
 fn swap_three_equal_pools_no_fees() {
@@ -256,7 +257,6 @@ fn swap_three_different_pools_no_fees() {
 
     // 5. check if it goes according to plan
     assert_eq!(token1.balance(&recipient), 0i128);
-    // because of the
     assert_eq!(
         token4.balance(&recipient),
         4_956i128,
@@ -264,148 +264,85 @@ fn swap_three_different_pools_no_fees() {
     );
 }
 
-// #[test]
-// fn swap_three_different_pools_with_fees() {
-//     let env = Env::default();
-//
-//     let admin = Address::random(&env);
-//
-//     env.mock_all_auths();
-//     env.budget().reset_unlimited();
-//
-//     let mut token1 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
-//     let mut token2 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
-//     let mut token3 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
-//     let mut token4 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
-//
-//     let mut tokens = [&mut token1, &mut token2, &mut token3, &mut token4];
-//     tokens.sort_by(|a, b| a.address.cmp(&b.address));
-//
-//     // 1. deploy factory
-//     let factory_client = deploy_and_initialize_factory(&env, admin.clone());
-//
-//     // 2. create liquidity pool from factory
-//     let lp_wasm_hash = install_lp_contract(&env);
-//
-//     let first_token_init_info = TokenInitInfo {
-//         token_wasm_hash: install_token_wasm(&env),
-//         token_a: tokens[0].address.clone(),
-//         token_b: tokens[1].address.clone(),
-//     };
-//     let first_stake_init_info = StakeInitInfo {
-//         stake_wasm_hash: install_stake_wasm(&env),
-//         min_bond: 10i128,
-//         max_distributions: 10u32,
-//         min_reward: 5i128,
-//     };
-//
-//     let first_lp_init_info = LiquidityPoolInitInfo {
-//         admin: admin.clone(),
-//         fee_recipient: admin.clone(),
-//         lp_wasm_hash: lp_wasm_hash.clone(),
-//         max_allowed_slippage_bps: 5000,
-//         max_allowed_spread_bps: 500,
-//         share_token_decimals: 7,
-//         swap_fee_bps: 1500,
-//         token_init_info: first_token_init_info.clone(),
-//         stake_init_info: first_stake_init_info,
-//     };
-//
-//     let second_token_init_info = TokenInitInfo {
-//         token_wasm_hash: install_token_wasm(&env),
-//         token_a: tokens[1].address.clone(),
-//         token_b: tokens[2].address.clone(),
-//     };
-//     let second_stake_init_info = StakeInitInfo {
-//         stake_wasm_hash: install_stake_wasm(&env),
-//         min_bond: 5i128,
-//         max_distributions: 5u32,
-//         min_reward: 2i128,
-//     };
-//
-//     let second_lp_init_info = LiquidityPoolInitInfo {
-//         admin: admin.clone(),
-//         fee_recipient: admin.clone(),
-//         lp_wasm_hash: lp_wasm_hash.clone(),
-//         max_allowed_slippage_bps: 4_000,
-//         max_allowed_spread_bps: 400,
-//         share_token_decimals: 6,
-//         swap_fee_bps: 1500,
-//         token_init_info: second_token_init_info,
-//         stake_init_info: second_stake_init_info,
-//     };
-//
-//     let third_token_init_info = TokenInitInfo {
-//         token_wasm_hash: install_token_wasm(&env),
-//         token_a: tokens[2].address.clone(),
-//         token_b: tokens[3].address.clone(),
-//     };
-//     let third_stake_init_info = StakeInitInfo {
-//         stake_wasm_hash: install_stake_wasm(&env),
-//         min_bond: 6i128,
-//         max_distributions: 6u32,
-//         min_reward: 3i128,
-//     };
-//
-//     let third_lp_init_info = LiquidityPoolInitInfo {
-//         admin: admin.clone(),
-//         fee_recipient: admin.clone(),
-//         lp_wasm_hash,
-//         max_allowed_slippage_bps: 4_000,
-//         max_allowed_spread_bps: 400,
-//         share_token_decimals: 6,
-//         swap_fee_bps: 1500,
-//         token_init_info: third_token_init_info,
-//         stake_init_info: third_stake_init_info,
-//     };
-//
-//     let first_lp = factory_client.create_liquidity_pool(&first_lp_init_info);
-//     let second_lp = factory_client.create_liquidity_pool(&second_lp_init_info);
-//     let third_lp = factory_client.create_liquidity_pool(&third_lp_init_info);
-//
-//     let pools = [first_lp, second_lp, third_lp];
-//
-//     // 3. provide liquidity for each one of the liquidity pools
-//     for pool in pools.iter() {
-//         let lp_client = lp_contract::Client::new(&env, pool);
-//         lp_client.provide_liquidity(
-//             &admin.clone(),
-//             &Some(1_000_000i128),
-//             &None,
-//             &Some(1_000_000i128),
-//             &None,
-//             &None::<i64>,
-//         );
-//     }
-//
-//     // 4. swap with multihop
-//     let multihop = deploy_multihop_contract(&env, admin.clone(), &factory_client.address);
-//     let recipient = Address::random(&env);
-//     tokens[0].mint(&recipient, &50i128);
-//     assert_eq!(tokens[0].balance(&recipient), 50i128);
-//     assert_eq!(tokens[3].balance(&recipient), 0i128);
-//
-//     let swap1 = Swap {
-//         offer_asset: tokens[0].address.clone(),
-//         ask_asset: tokens[1].address.clone(),
-//     };
-//     let swap2 = Swap {
-//         offer_asset: tokens[1].address.clone(),
-//         ask_asset: tokens[2].address.clone(),
-//     };
-//     let swap3 = Swap {
-//         offer_asset: tokens[2].address.clone(),
-//         ask_asset: tokens[3].address.clone(),
-//     };
-//
-//     let operations = vec![&env, swap1, swap2, swap3];
-//
-//     multihop.swap(&recipient, &operations, &50i128);
-//
-//     // 5. check if it goes according to plan
-//     assert_eq!(tokens[0].balance(&recipient), 0i128);
-//     assert_eq!(tokens[3].balance(&recipient), 247i128);
-// }
+#[test]
+fn swap_three_different_pools_with_fees() {
+    let env = Env::default();
+
+    let admin = Address::random(&env);
+
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let token1 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
+    let token2 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
+    let token3 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
+    let token4 = deploy_and_mint_tokens(&env, &admin, 10_000_000i128);
+
+    // 1. deploy factory
+    let factory_client = deploy_and_initialize_factory(&env, admin.clone());
+
+    deploy_and_initialize_lp(
+        &env,
+        &factory_client,
+        admin.clone(),
+        token1.address.clone(),
+        1_000_000,
+        token2.address.clone(),
+        1_000_000,
+        Some(2000),
+    );
+    deploy_and_initialize_lp(
+        &env,
+        &factory_client,
+        admin.clone(),
+        token2.address.clone(),
+        2_000_000,
+        token3.address.clone(),
+        2_000_000,
+        Some(2000),
+    );
+    deploy_and_initialize_lp(
+        &env,
+        &factory_client,
+        admin.clone(),
+        token3.address.clone(),
+        3_000_000,
+        token4.address.clone(),
+        3_000_000,
+        Some(2000),
+    );
+
+    // 4. swap with multihop
+    let multihop = deploy_multihop_contract(&env, admin.clone(), &factory_client.address);
+    let recipient = Address::random(&env);
+    token1.mint(&recipient, &5000i128);
+    assert_eq!(token1.balance(&recipient), 5000i128);
+    assert_eq!(token4.balance(&recipient), 0i128);
+
+    let swap1 = Swap {
+        offer_asset: token1.address.clone(),
+        ask_asset: token2.address.clone(),
+    };
+    let swap2 = Swap {
+        offer_asset: token2.address.clone(),
+        ask_asset: token3.address.clone(),
+    };
+    let swap3 = Swap {
+        offer_asset: token3.address.clone(),
+        ask_asset: token4.address.clone(),
+    };
+
+    let operations = vec![&env, swap1, swap2, swap3];
+
+    multihop.swap(&recipient, &operations, &5000i128);
+
+    // we star swapping from 5000 tokens
+    // first swap we end up with 5000 - 20% = 4000
+    // second swap we end up with 4000 - 20% = 3200
+    // finally we end up with 3200 - 20% = 2560
+    assert_eq!(token1.balance(&recipient), 0i128);
+    assert_eq!(token4.balance(&recipient), 2560i128);
+}
 
 #[test]
 fn swap_panics_with_no_operations() {
@@ -462,6 +399,7 @@ fn deploy_and_initialize_lp(
         std::mem::swap(&mut token_a, &mut token_b);
         std::mem::swap(&mut token_a_amount, &mut token_b_amount);
     }
+
     let token_init_info = TokenInitInfo {
         token_wasm_hash: install_token_wasm(env),
         token_a: token_a.clone(),
