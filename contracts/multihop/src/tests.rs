f13f0d461999a3f93e480530640b13744668dc48
Kaloyan Gangov <gangov1@gmail.com>
2023-10-27 21:41:46 +0300

    Multihop: moves the tests from verify_operations.rs to utils.rs and adds two more tests for reverse_swap

--- contracts/multihop/src/tests.rs
+++ contracts/multihop/src/tests.rs
@@ -5,7 +5,6 @@ use soroban_sdk::{testutils::Address as _, Address, Env};
 mod query;
 mod setup;
 mod swap;
-mod verify_operations;
 
 #[test]
 #[should_panic(expected = "Multihop: Initialize: initializing contract twice is not allowed")]
