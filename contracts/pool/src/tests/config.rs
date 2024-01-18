8cb67c57143cf98f2452f107e5f3120763d045c1
Kaloyan Gangov <gangov1@gmail.com>
2024-01-18 22:06:20 +0200

    Pool: rebase and addresses comments

--- contracts/pool/src/tests/config.rs
+++ contracts/pool/src/tests/config.rs
@@ -76,7 +76,7 @@ fn update_config() {
     );
 
     // update slippage and spread
-    pool.update_config(&None, &None, &None, &Some(5_000i64), &Some(500));
+    pool.update_config(&None, &None, &None, &None, &Some(5_000i64), &Some(500));
     assert_eq!(
         pool.query_config(),
         Config {
@@ -87,15 +87,15 @@ fn update_config() {
             pool_type: PairType::Xyk,
             total_fee_bps: 500,
             fee_recipient: admin2,
-            max_allowed_slippage_bps: 5_000,
-            max_allowed_spread_bps: 500,
-            max_referral_bps: 1_000,
+            max_allowed_slippage_bps: 500,
+            max_allowed_spread_bps: 5_000,
+            max_referral_bps: 500,
         }
     );
 }
 
 #[test]
-#[should_panic]
+#[should_panic(expected = "Error(Auth, InvalidAction)")]
 fn update_config_unauthorized() {
     let env = Env::default();
 
@@ -158,13 +158,13 @@ fn update_config_update_admin() {
     );
 
     // update admin to new admin
-    pool.update_config(&Some(admin2.clone()), &None, &None, &None, &None);
+    pool.update_config(&Some(admin2.clone()), &None, &None, &None, &None, &None);
 
     let share_token_address = pool.query_share_token_address();
     let stake_token_address = pool.query_stake_contract_address();
 
     // now update succeeds
-    pool.update_config(&None, &None, &None, &None, &Some(3_000_000));
+    pool.update_config(&Some(admin2.clone()), &None, &None, &None, &None, &None);
     assert_eq!(
         pool.query_config(),
         Config {
@@ -176,7 +176,7 @@ fn update_config_update_admin() {
             total_fee_bps: 0,
             fee_recipient: user1,
             max_allowed_slippage_bps: 500,
-            max_allowed_spread_bps: 3_000_000,
+            max_allowed_spread_bps: 200,
             max_referral_bps: 5_000,
         }
     );
