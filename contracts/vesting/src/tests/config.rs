f9f154dd080a52bb2658c638b1a41bb16ba1ad91
Kaloyan Gangov <gangov1@gmail.com>
2024-04-10 12:26:08 +0300

    vesting: covers a test case where we instantiate the contract with empty list of whitelisted accoutns

--- contracts/vesting/src/tests/config.rs
+++ contracts/vesting/src/tests/config.rs
@@ -86,6 +86,80 @@ fn instantiate_contract_succesffuly() {
     );
 }
 
+#[test]
+fn instantiate_contract_succesffuly_with_empty_list_of_whitelisted_accounts() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let admin = Address::generate(&env);
+    let vester1 = Address::generate(&env);
+    let vester2 = Address::generate(&env);
+
+    let vesting_token = VestingTokenInfo {
+        name: String::from_str(&env, "Phoenix"),
+        symbol: String::from_str(&env, "PHO"),
+        decimals: 6,
+        address: Address::generate(&env),
+        total_supply: 0,
+    };
+    let vesting_balances = vec![
+        &env,
+        VestingBalance {
+            address: vester1,
+            balance: 100,
+            curve: Curve::SaturatingLinear(SaturatingLinear {
+                min_x: 15,
+                min_y: 1,
+                max_x: 60,
+                max_y: 120,
+            }),
+        },
+        VestingBalance {
+            address: vester2,
+            balance: 100,
+            curve: Curve::SaturatingLinear(SaturatingLinear {
+                min_x: 30,
+                min_y: 2,
+                max_x: 120,
+                max_y: 240,
+            }),
+        },
+    ];
+
+    let minter_info = &MinterInfo {
+        address: Address::generate(&env),
+        cap: Curve::SaturatingLinear(SaturatingLinear {
+            min_x: 30,
+            min_y: 2,
+            max_x: 120,
+            max_y: 240,
+        }),
+    };
+
+    let allowed_vesters = vec![&env];
+
+    let vesting_client = instantiate_vesting_client(&env);
+    env.ledger().with_mut(|li| li.timestamp = 1000);
+    vesting_client.initialize(
+        &admin,
+        &vesting_token,
+        &vesting_balances,
+        minter_info,
+        &Some(allowed_vesters),
+        &10u32,
+    );
+
+    assert_eq!(
+        vesting_client.query_config(),
+        Config {
+            admin,
+            whitelist: vec![&env],
+            token_info: vesting_token,
+            max_vesting_complexity: 10,
+        }
+    );
+}
+
 #[should_panic(expected = "Vesting: Initialize: At least one balance must be provided.")]
 #[test]
 fn instantiate_contract_without_any_vesting_balances_should_fail() {
