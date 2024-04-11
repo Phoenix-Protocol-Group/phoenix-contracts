cc22da405d43743fa108a48da140756fb27c6b15
Kaloyan Gangov <gangov1@gmail.com>
2024-04-11 11:08:33 +0300

    vesting: extends the tests

--- contracts/vesting/src/tests/instantiate.rs
+++ contracts/vesting/src/tests/instantiate.rs
@@ -78,6 +78,64 @@ fn instantiate_contract_succesffuly() {
 }
 
 #[test]
+fn instantiate_contract_succesffuly_with_constant_curve_minter_info() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let admin = Address::generate(&env);
+    let vester1 = Address::generate(&env);
+    let whitelisted_account = Address::generate(&env);
+
+    let vesting_token = VestingTokenInfo {
+        name: String::from_str(&env, "Phoenix"),
+        symbol: String::from_str(&env, "PHO"),
+        decimals: 6,
+        address: Address::generate(&env),
+        total_supply: 240,
+    };
+    let vesting_balances = vec![
+        &env,
+        VestingBalance {
+            address: vester1,
+            balance: 240,
+            curve: Curve::SaturatingLinear(SaturatingLinear {
+                min_x: 15,
+                min_y: 120,
+                max_x: 60,
+                max_y: 0,
+            }),
+        },
+    ];
+
+    let allowed_vesters = vec![&env, whitelisted_account.clone()];
+    let minter_info = MinterInfo {
+        address: Address::generate(&env),
+        cap: Curve::Constant(511223344),
+    };
+
+    let vesting_client = instantiate_vesting_client(&env);
+    env.ledger().with_mut(|li| li.timestamp = 1000);
+    vesting_client.initialize(
+        &admin,
+        &vesting_token,
+        &vesting_balances,
+        &Some(minter_info),
+        &Some(allowed_vesters),
+        &10u32,
+    );
+
+    assert_eq!(
+        vesting_client.query_config(),
+        Config {
+            admin,
+            whitelist: vec![&env, whitelisted_account],
+            token_info: vesting_token,
+            max_vesting_complexity: 10,
+        }
+    );
+}
+
+#[test]
 fn instantiate_contract_succesffuly_with_empty_list_of_whitelisted_accounts() {
     let env = Env::default();
     env.mock_all_auths();
