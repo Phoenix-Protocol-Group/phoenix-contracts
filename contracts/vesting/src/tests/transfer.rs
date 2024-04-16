714a44ebf79e30cf37189d14d3d6e68cf672d89a
Kaloyan Gangov <gangov1@gmail.com>
2024-04-16 15:11:39 +0300

    vesting: asserting error msgs

--- contracts/vesting/src/tests/transfer.rs
+++ contracts/vesting/src/tests/transfer.rs
@@ -1,4 +1,5 @@
 use curve::{Curve, SaturatingLinear};
+use soroban_sdk::testutils::arbitrary::std::dbg;
 use soroban_sdk::{
     testutils::{Address as _, Ledger},
     vec, Address, Env, String,
@@ -75,16 +76,12 @@ fn transfer_tokens_should_fail_invalid_amount() {
     let admin = Address::generate(&env);
     let vester1 = Address::generate(&env);
     let vester2 = Address::generate(&env);
-    let whitelisted_account = Address::generate(&env);
-    let token = deploy_token_contract(&env, &admin);
-
-    token.mint(&vester1, &1_000);
 
     let vesting_token = VestingTokenInfo {
         name: String::from_str(&env, "Phoenix"),
         symbol: String::from_str(&env, "PHO"),
         decimals: 6,
-        address: token.address.clone(),
+        address: Address::generate(&env),
         total_supply: 0,
     };
     let vesting_balances = vec![
@@ -101,23 +98,18 @@ fn transfer_tokens_should_fail_invalid_amount() {
         },
     ];
 
-    let allowed_vesters = vec![&env, whitelisted_account.clone()];
-
     let vesting_client = instantiate_vesting_client(&env);
-    env.ledger().with_mut(|li| li.timestamp = 1000);
+
     vesting_client.initialize(
         &admin,
         &vesting_token,
         &vesting_balances,
         &None,
-        &Some(allowed_vesters),
+        &None,
         &10u32,
     );
 
-    assert_eq!(token.balance(&vester2), 0);
     vesting_client.transfer_token(&vester1, &vester2, &0);
-    assert_eq!(vesting_client.query_balance(&vester1), 900);
-    assert_eq!(token.balance(&vester2), 100);
 }
 
 #[test]
@@ -210,4 +202,126 @@ fn transfer_vesting_works() {
     );
 
     // TODO: add more tests for this
+}
+
+#[test]
+#[should_panic(expected = "Vesting: Transfer Vesting: Not authorized to transfer vesting")]
+fn transfer_vesting_should_fail_when_sender_not_in_auth_list() {
+    let env = Env::default();
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let admin = Address::generate(&env);
+    let vester1 = Address::generate(&env);
+    let vester2 = Address::generate(&env);
+    let token = deploy_token_contract(&env, &admin);
+
+    token.mint(&vester1, &1_000);
+
+    let vesting_token = VestingTokenInfo {
+        name: String::from_str(&env, "Phoenix"),
+        symbol: String::from_str(&env, "PHO"),
+        decimals: 6,
+        address: token.address.clone(),
+        total_supply: 0,
+    };
+    let vesting_balances = vec![
+        &env,
+        VestingBalance {
+            address: vester1.clone(),
+            balance: 200,
+            curve: Curve::SaturatingLinear(SaturatingLinear {
+                min_x: 15,
+                min_y: 120,
+                max_x: 60,
+                max_y: 0,
+            }),
+        },
+    ];
+
+    let allowed_vesters = vec![&env, Address::generate(&env)];
+
+    let vesting_client = instantiate_vesting_client(&env);
+    vesting_client.initialize(
+        &admin,
+        &vesting_token,
+        &vesting_balances,
+        &None,
+        &Some(allowed_vesters),
+        &10u32,
+    );
+
+    vesting_client.transfer_vesting(
+        &vester1,
+        &vester2,
+        &200,
+        &Curve::SaturatingLinear(SaturatingLinear {
+            min_x: 15,
+            min_y: 120,
+            max_x: 60,
+            max_y: 0,
+        }),
+    );
+}
+
+#[test]
+#[should_panic(expected = "Vesting: Transfer Vesting: Invalid low value")]
+fn transfer_vesting_should_fail_when_invalid_low_value() {
+    let env = Env::default();
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let admin = Address::generate(&env);
+    let vester1 = Address::generate(&env);
+    let vester2 = Address::generate(&env);
+    let token = deploy_token_contract(&env, &admin);
+
+    token.mint(&vester1, &1_000);
+
+    let vesting_token = VestingTokenInfo {
+        name: String::from_str(&env, "Phoenix"),
+        symbol: String::from_str(&env, "PHO"),
+        decimals: 6,
+        address: token.address.clone(),
+        total_supply: 0,
+    };
+    let vesting_balances = vec![
+        &env,
+        VestingBalance {
+            address: vester1.clone(),
+            balance: 200,
+            curve: Curve::SaturatingLinear(SaturatingLinear {
+                min_x: 15,
+                min_y: 120,
+                max_x: 60,
+                max_y: 0,
+            }),
+        },
+    ];
+
+    let allowed_vesters = vec![&env, vester1.clone()];
+
+    let vesting_client = instantiate_vesting_client(&env);
+    dbg!("before init");
+    vesting_client.initialize(
+        &admin,
+        &vesting_token,
+        &vesting_balances,
+        &None,
+        &Some(allowed_vesters),
+        &10u32,
+    );
+    dbg!("after init");
+
+    vesting_client.transfer_vesting(
+        &vester1,
+        &vester2,
+        &500,
+        &Curve::SaturatingLinear(SaturatingLinear {
+            min_x: 15,
+            min_y: 120,
+            max_x: 60,
+            max_y: 60,
+        }),
+    );
 }
