a7d354ce96d6f7f5cbdfde9487f248facf22acd3
Jakub <jakub@moonbite.space>
2024-05-21 16:17:50 +0200

    Vesting: Refactor instantiate tests

--- contracts/vesting/src/tests/instantiate.rs
+++ contracts/vesting/src/tests/instantiate.rs
@@ -95,39 +95,40 @@ fn instantiate_contract_without_any_vesting_balances_should_fail() {
     vesting_client.create_vesting_schedules(&vesting_schedules);
 }
 
-// #[should_panic(
-//     expected = "Vesting: Create vesting account: Admin does not have enough tokens to start the vesting schedule"
-// )]
-// #[test]
-// fn create_schedule_panics_when_admin_has_no_tokens_to_fund() {
-//     let env = Env::default();
-//     env.mock_all_auths();
-//
-//     let admin = Address::generate(&env);
-//     let vester1 = Address::generate(&env);
-//
-//     let token_client = deploy_token_contract(&env, &admin);
-//
-//     let vesting_token = VestingTokenInfo {
-//         name: String::from_str(&env, "Phoenix"),
-//         symbol: String::from_str(&env, "PHO"),
-//         decimals: 6,
-//         address: token_client.address.clone(),
-//     };
-//     let vesting_schedules = vec![
-//         &env,
-//         VestingSchedule {
-//             recipient: vester1,
-//             distribution_info: DistributionInfo {
-//                 start_timestamp: 15,
-//                 end_timestamp: 60,
-//                 amount: 120,
-//             },
-//         },
-//     ];
-//
-//     let vesting_client = instantiate_vesting_client(&env);
-//
-//     vesting_client.initialize(&admin, &vesting_token, &10u32);
-//     vesting_client.create_vesting_schedules(&vesting_schedules);
-// }
+#[should_panic(
+    expected = "Vesting: Create vesting account: Admin does not have enough tokens to start the vesting schedule"
+)]
+#[test]
+fn create_schedule_panics_when_admin_has_no_tokens_to_fund() {
+    let env = Env::default();
+    env.mock_all_auths();
+
+    let admin = Address::generate(&env);
+    let vester1 = Address::generate(&env);
+
+    let token_client = deploy_token_contract(&env, &admin);
+
+    let vesting_token = VestingTokenInfo {
+        name: String::from_str(&env, "Phoenix"),
+        symbol: String::from_str(&env, "PHO"),
+        decimals: 6,
+        address: token_client.address.clone(),
+    };
+    let vesting_schedules = vec![
+        &env,
+        VestingSchedule {
+            recipient: vester1,
+            curve: Curve::SaturatingLinear(SaturatingLinear {
+                min_x: 15,
+                min_y: 120,
+                max_x: 60,
+                max_y: 0,
+            }),
+        },
+    ];
+
+    let vesting_client = instantiate_vesting_client(&env);
+
+    vesting_client.initialize(&admin, &vesting_token, &10u32);
+    vesting_client.create_vesting_schedules(&vesting_schedules);
+}
