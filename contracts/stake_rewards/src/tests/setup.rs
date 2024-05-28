02a73612fad1c65e8b917235b305a6021ef54124
Jakub <jakub@moonbite.space>
2024-05-28 12:19:05 +0200

    Stake rewards: Adjust the test setup harness

--- contracts/stake_rewards/src/tests/setup.rs
+++ contracts/stake_rewards/src/tests/setup.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{testutils::Address as _, Address, Env};
+use soroban_sdk::{Address, Env};
 
 use crate::{
     contract::{StakingRewards, StakingRewardsClient},
@@ -10,7 +10,7 @@ pub fn deploy_token_contract<'a>(env: &Env, admin: &Address) -> token_contract::
 }
 
 pub fn deploy_stake_contract<'a>(env: &Env, admin: &Address) -> stake_contract::Client<'a> {
-    token_contract::Client::new(env, &env.register_stellar_asset_contract(admin.clone()))
+    stake_contract::Client::new(env, &env.register_stellar_asset_contract(admin.clone()))
 }
 
 const MIN_BOND: i128 = 1000;
@@ -25,7 +25,7 @@ pub fn deploy_staking_rewards_contract<'a>(
     lp_token: &Address,
     reward_token: &Address,
     owner: &Address,
-) -> (StakingClient<'a>, StakingRewardsClient<'a>) {
+) -> (stake_contract::Client<'a>, StakingRewardsClient<'a>) {
     let staking = deploy_stake_contract(env, admin);
     staking.initialize(
         &admin,
@@ -34,12 +34,19 @@ pub fn deploy_staking_rewards_contract<'a>(
         &MIN_REWARD,
         admin,
         owner,
-        MAX_COMPLEXITY,
+        &MAX_COMPLEXITY,
     );
 
     let staking_rewards =
         StakingRewardsClient::new(env, &env.register_contract(None, StakingRewards {}));
 
-    staking_rewards.initialize(&admin, staking.address, reward_token, owner);
+    staking_rewards.initialize(
+        &admin,
+        &staking.address,
+        reward_token,
+        &MAX_COMPLEXITY,
+        &MIN_REWARD,
+        &MIN_BOND,
+    );
     (staking, staking_rewards)
 }
