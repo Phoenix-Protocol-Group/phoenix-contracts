582c684383023b78c05197252607febd37452fb3
Jakub <jakub@moonbite.space>
2024-06-05 14:15:17 +0200

    Stake rewards: Test for pay rewards during unbond

--- contracts/stake_rewards/src/tests/bond.rs
+++ contracts/stake_rewards/src/tests/bond.rs
@@ -162,137 +162,59 @@ fn calculate_unbond_one_user() {
     );
 }
 
-// #[test]
-// fn initializing_contract_sets_total_staked_var() {
-//     let env = Env::default();
-//     env.mock_all_auths();
-//
-//     let admin = Address::generate(&env);
-//     let manager = Address::generate(&env);
-//     let owner = Address::generate(&env);
-//     let lp_token = deploy_token_contract(&env, &admin);
-//
-//     let staking = deploy_staking_contract(
-//         &env,
-//         admin.clone(),
-//         &lp_token.address,
-//         &manager,
-//         &owner,
-//         &DEFAULT_COMPLEXITY,
-//     );
-//
-//     assert_eq!(staking.query_total_staked(), 0);
-// }
-//
-// #[test]
-// #[should_panic(expected = "Stake: Remove stake: Stake not found")]
-// fn unbond_wrong_user_stake_not_found() {
-//     let env = Env::default();
-//     env.mock_all_auths();
-//
-//     let admin = Address::generate(&env);
-//     let user = Address::generate(&env);
-//     let user2 = Address::generate(&env);
-//     let manager = Address::generate(&env);
-//     let owner = Address::generate(&env);
-//     let lp_token = deploy_token_contract(&env, &admin);
-//
-//     let staking = deploy_staking_contract(
-//         &env,
-//         admin.clone(),
-//         &lp_token.address,
-//         &manager,
-//         &owner,
-//         &DEFAULT_COMPLEXITY,
-//     );
-//
-//     lp_token.mint(&user, &35_000);
-//     lp_token.mint(&user2, &10_000);
-//
-//     env.ledger().with_mut(|li| {
-//         li.timestamp = ONE_DAY;
-//     });
-//     staking.bond(&user, &10_000);
-//     env.ledger().with_mut(|li| {
-//         li.timestamp += ONE_DAY;
-//     });
-//     staking.bond(&user, &10_000);
-//     staking.bond(&user2, &10_000);
-//
-//     assert_eq!(lp_token.balance(&user), 15_000);
-//     assert_eq!(lp_token.balance(&user2), 0);
-//     assert_eq!(lp_token.balance(&staking.address), 30_000);
-//
-//     let non_existing_timestamp = ONE_DAY / 2;
-//     staking.unbond(&user2, &10_000, &non_existing_timestamp);
-// }
-//
-// #[test]
-// fn pay_rewards_during_unbond() {
-//     const STAKED_AMOUNT: i128 = 1_000;
-//     let env = Env::default();
-//     env.mock_all_auths();
-//
-//     let admin = Address::generate(&env);
-//     let user = Address::generate(&env);
-//     let manager = Address::generate(&env);
-//     let owner = Address::generate(&env);
-//
-//     let lp_token = deploy_token_contract(&env, &admin);
-//     let reward_token = deploy_token_contract(&env, &admin);
-//     let staking = deploy_staking_contract(
-//         &env,
-//         admin.clone(),
-//         &lp_token.address,
-//         &manager,
-//         &owner,
-//         &DEFAULT_COMPLEXITY,
-//     );
-//
-//     lp_token.mint(&user, &10_000);
-//     reward_token.mint(&admin, &10_000);
-//
-//     env.ledger().with_mut(|li| {
-//         li.timestamp = ONE_WEEK;
-//     });
-//
-//     staking.create_distribution_flow(&manager, &reward_token.address);
-//     staking.fund_distribution(
-//         &admin,
-//         &ONE_WEEK,
-//         &10_000u64,
-//         &reward_token.address,
-//         &10_000,
-//     );
-//
-//     env.ledger().with_mut(|li| {
-//         li.timestamp = ONE_WEEK + 5_000;
-//     });
-//     staking.bond(&user, &STAKED_AMOUNT);
-//
-//     staking.distribute_rewards();
-//
-//     // user has bonded for 5_000 time, initial rewards are 10_000
-//     // so user should have 5_000 rewards
-//     // 5_000 rewards are still undistributed
-//     assert_eq!(
-//         staking.query_undistributed_rewards(&reward_token.address),
-//         5_000
-//     );
-//     assert_eq!(
-//         staking
-//             .query_withdrawable_rewards(&user)
-//             .rewards
-//             .iter()
-//             .map(|reward| reward.reward_amount)
-//             .sum::<u128>(),
-//         5_000
-//     );
-//     assert_eq!(reward_token.balance(&user), 0);
-//     staking.unbond(&user, &STAKED_AMOUNT, &(ONE_WEEK + 5_000));
-//     assert_eq!(reward_token.balance(&user), 5_000);
-// }
-//
+#[test]
+fn pay_rewards_during_calculate_unbond() {
+    let env = Env::default();
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let admin = Address::generate(&env);
+    let lp_token = deploy_token_contract(&env, &admin);
+    let reward_token = deploy_token_contract(&env, &admin);
+
+    let (staking, staking_rewards) =
+        deploy_staking_rewards_contract(&env, &admin, &lp_token.address, &reward_token.address);
+    assert_eq!(staking.query_total_staked(), 0);
+
+    let start_timestamp = 100;
+    env.ledger().with_mut(|li| {
+        li.timestamp = start_timestamp;
+    });
+
+    reward_token.mint(&admin, &1_000_000);
+    let reward_duration = 600;
+    staking_rewards.fund_distribution(&admin, &start_timestamp, &reward_duration, &1_000_000);
+
+    let user1 = Address::generate(&env);
+    lp_token.mint(&user1, &10_000);
+    assert_eq!(lp_token.balance(&user1), 10_000);
+    assert_eq!(lp_token.balance(&staking.address), 0);
+    assert_eq!(staking.query_config().config.lp_token, lp_token.address);
+    staking.bond(&user1, &10_000);
+
+    staking_rewards.calculate_bond(&user1);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = start_timestamp + reward_duration; // move to the end of the distribution
+    });
+
+    staking_rewards.distribute_rewards();
+
+    assert_eq!(
+        staking_rewards.query_undistributed_reward(&reward_token.address),
+        0
+    );
+    assert_eq!(
+        staking_rewards.query_distributed_reward(&reward_token.address),
+        1_000_000
+    );
+
+    // unbonding and automatically withdraws rewards
+    staking_rewards.calculate_unbond(&user1);
+    staking.unbond(&user1, &10_000, &start_timestamp);
+    assert_eq!(reward_token.balance(&user1), 1_000_000);
+}
+
 // #[should_panic(
 //     expected = "Stake: initialize: Minimum amount of lp share tokens to bond can not be smaller or equal to 0"
 // )]
