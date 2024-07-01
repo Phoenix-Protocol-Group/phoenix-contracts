1d316bfefc7d4b5c2a827ec9a937f04e27109e9f
Kaloyan Gangov <gangov1@gmail.com>
2024-07-01 12:42:55 +0300

    lints and adds changes to tests to verify that the update is working

--- contracts/pool/src/tests/config.rs
+++ contracts/pool/src/tests/config.rs
@@ -127,6 +127,7 @@ fn update_config() {
         &None,
         &None,
         &Some(1_000i64),
+        &Some(100),
     );
     assert_eq!(
         pool.query_config(),
@@ -147,7 +148,15 @@ fn update_config() {
     );
 
     // update slippage and spread
-    pool.update_config(&None, &None, &None, &None, &Some(5_000i64), &Some(500));
+    pool.update_config(
+        &None,
+        &None,
+        &None,
+        &None,
+        &Some(5_000i64),
+        &Some(500),
+        &None,
+    );
     assert_eq!(
         pool.query_config(),
         Config {
@@ -205,6 +214,7 @@ fn update_config_unauthorized() {
         &None,
         &None,
         &None,
+        &None,
     );
 }
 
@@ -241,13 +251,29 @@ fn update_config_update_admin() {
     );
 
     // update admin to new admin
-    pool.update_config(&Some(admin2.clone()), &None, &None, &None, &None, &None);
+    pool.update_config(
+        &Some(admin2.clone()),
+        &None,
+        &None,
+        &None,
+        &None,
+        &None,
+        &None,
+    );
 
     let share_token_address = pool.query_share_token_address();
     let stake_token_address = pool.query_stake_contract_address();
 
     // now update succeeds
-    pool.update_config(&Some(admin2.clone()), &None, &None, &None, &None, &None);
+    pool.update_config(
+        &Some(admin2.clone()),
+        &None,
+        &None,
+        &None,
+        &None,
+        &None,
+        &None,
+    );
     assert_eq!(
         pool.query_config(),
         Config {
@@ -304,6 +330,7 @@ fn update_config_too_high_fees() {
         &None,
         &Some(10_100i64), // 101% fees
         &Some(admin2.clone()),
+        &None,
         &None,
         &None,
         &None,
