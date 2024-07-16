d296b0dd397abb225fb2d3df9608653a3c8dce51
Kaloyan Gangov <gangov1@gmail.com>
2024-07-16 12:08:57 +0300

    adds more tests

--- contracts/stake/src/tests/bond.rs
+++ contracts/stake/src/tests/bond.rs
@@ -1,7 +1,10 @@
+extern crate std;
+
 use pretty_assertions::assert_eq;
 use soroban_sdk::{
-    testutils::{Address as _, Ledger},
-    vec, Address, Env,
+    symbol_short,
+    testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation, Ledger},
+    vec, Address, Env, IntoVal, Symbol,
 };
 
 use super::setup::{deploy_staking_contract, deploy_token_contract};
@@ -138,6 +141,28 @@ fn bond_simple() {
 
     staking.bond(&user, &10_000);
 
+    assert_eq!(
+        env.auths(),
+        [(
+            user.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    staking.address.clone(),
+                    Symbol::new(&env, "bond"),
+                    (&user.clone(), 10_000i128,).into_val(&env),
+                )),
+                sub_invocations: std::vec![AuthorizedInvocation {
+                    function: AuthorizedFunction::Contract((
+                        lp_token.address.clone(),
+                        symbol_short!("transfer"),
+                        (&user, &staking.address.clone(), 10_000i128).into_val(&env)
+                    )),
+                    sub_invocations: std::vec![],
+                },],
+            }
+        ),]
+    );
+
     let bonds = staking.query_staked(&user).stakes;
     assert_eq!(
         bonds,
@@ -198,6 +223,21 @@ fn unbond_simple() {
     assert_eq!(lp_token.balance(&staking.address), 45_000);
 
     staking.unbond(&user, &10_000, &(ONE_DAY + ONE_DAY));
+
+    assert_eq!(
+        env.auths(),
+        [(
+            user.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    staking.address.clone(),
+                    Symbol::new(&env, "unbond"),
+                    (&user.clone(), 10_000i128, (ONE_DAY + ONE_DAY)).into_val(&env),
+                )),
+                sub_invocations: std::vec![],
+            }
+        ),]
+    );
 
     let bonds = staking.query_staked(&user).stakes;
     assert_eq!(
