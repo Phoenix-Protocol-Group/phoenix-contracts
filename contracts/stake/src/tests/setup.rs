081339339a821f15ccfd8dc282dfd93fb2b96574
Jakub <jakub@moonbite.space>
2024-08-01 15:40:28 +0200

    Stake: Add stake_rewards to test setup harness

--- contracts/stake/src/tests/setup.rs
+++ contracts/stake/src/tests/setup.rs
@@ -1,4 +1,4 @@
-use soroban_sdk::{testutils::Address as _, Address, Env};
+use soroban_sdk::{testutils::Address as _, Address, BytesN, Env};
 
 use crate::{
     contract::{Staking, StakingClient},
@@ -7,6 +7,14 @@ use crate::{
 
 pub fn deploy_token_contract<'a>(env: &Env, admin: &Address) -> token_contract::Client<'a> {
     token_contract::Client::new(env, &env.register_stellar_asset_contract(admin.clone()))
+}
+
+#[allow(clippy::too_many_arguments)]
+pub fn install_stake_rewards_contract(env: &Env) -> BytesN<32> {
+    soroban_sdk::contractimport!(
+        file = "../../target/wasm32-unknown-unknown/release/phoenix_stake_rewards.wasm"
+    );
+    env.deployer().upload_contract_wasm(WASM)
 }
 
 const MIN_BOND: i128 = 1000;
@@ -24,10 +32,12 @@ pub fn deploy_staking_contract<'a>(
 ) -> StakingClient<'a> {
     let admin = admin.into().unwrap_or(Address::generate(env));
     let staking = StakingClient::new(env, &env.register_contract(None, Staking {}));
+    let stake_rewards_hash = install_stake_rewards_contract(env);
 
     staking.initialize(
         &admin,
         lp_token,
+        &stake_rewards_hash,
         &MIN_BOND,
         &MIN_REWARD,
         manager,
