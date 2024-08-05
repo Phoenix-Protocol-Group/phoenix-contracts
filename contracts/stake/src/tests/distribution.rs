cd17f418724de8234cd232d56e74ce04e91ea990
Jakub <jakub@moonbite.space>
2024-08-05 13:01:59 +0200

    Stake: add test case for the different user reward multipliers

--- contracts/stake/src/tests/distribution.rs
+++ contracts/stake/src/tests/distribution.rs
@@ -1289,3 +1289,102 @@ fn panic_when_funding_distribution_with_curve_too_complex() {
         &1000,
     );
 }
+
+#[test]
+fn multiple_equal_users_with_different_multipliers() {
+    let env = Env::default();
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let admin = Address::generate(&env);
+    let manager = Address::generate(&env);
+    let lp_token = deploy_token_contract(&env, &admin);
+    let reward_token = deploy_token_contract(&env, &admin);
+
+    let staking = deploy_staking_contract(
+        &env,
+        admin.clone(),
+        &lp_token.address,
+        &manager,
+        &admin,
+        &50u32,
+    );
+    staking.create_distribution_flow(
+        &admin,
+        &reward_token.address,
+        &BytesN::from_array(&env, &[1; 32]),
+        &10,
+        &100,
+        &1,
+    );
+
+    // first user bonds at timestamp 0
+    // he will get 100% of his rewards
+    let user1 = Address::generate(&env);
+    lp_token.mint(&user1, &10_000);
+    staking.bond(&user1, &10_000);
+
+    let fifteen_days = 3600 * 24 * 15;
+    env.ledger().with_mut(|li| {
+        li.timestamp = fifteen_days;
+    });
+
+    // user2 will receive 75% of his reward
+    let user2 = Address::generate(&env);
+    lp_token.mint(&user2, &10_000);
+    staking.bond(&user2, &10_000);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = fifteen_days * 2;
+    });
+
+    // user3 will receive 50% of his reward
+    let user3 = Address::generate(&env);
+    lp_token.mint(&user3, &10_000);
+    staking.bond(&user3, &10_000);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = fifteen_days * 3;
+    });
+
+    // user4 will receive 25% of his reward
+    let user4 = Address::generate(&env);
+    lp_token.mint(&user4, &10_000);
+    staking.bond(&user4, &10_000);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp = fifteen_days * 4;
+    });
+
+    reward_token.mint(&admin, &1_000_000);
+    // reward distribution starts at the latest timestamp and lasts just 1 second
+    // the point is to prove that the multiplier works correctly
+    staking.fund_distribution(&(fifteen_days * 4), &1, &reward_token.address, &1_000_000);
+
+    env.ledger().with_mut(|li| {
+        li.timestamp += 1;
+    });
+
+    staking.distribute_rewards();
+
+    // The way it works - contract will treat all the funds as distributed, and the amount
+    // that was not sent due to low staking bonus stays on the contract
+
+    assert_eq!(
+        staking.query_undistributed_rewards(&reward_token.address),
+        0
+    );
+    assert_eq!(
+        staking.query_distributed_rewards(&reward_token.address),
+        1_000_000
+    );
+
+    staking.withdraw_rewards(&user1);
+    assert_eq!(reward_token.balance(&user1), 250_000);
+    staking.withdraw_rewards(&user2);
+    assert_eq!(reward_token.balance(&user2), 187_500);
+    staking.withdraw_rewards(&user3);
+    assert_eq!(reward_token.balance(&user3), 125_000);
+    staking.withdraw_rewards(&user4);
+    assert_eq!(reward_token.balance(&user4), 62_500);
+}
