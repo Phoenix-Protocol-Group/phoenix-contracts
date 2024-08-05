2cf83aa8bffc69f14a20a1a8735532dd5acd5ee6
Jakub <jakub@moonbite.space>
2024-08-05 10:32:00 +0200

    Stake: Four users with different stakes

--- contracts/stake/src/tests/distribution.rs
+++ contracts/stake/src/tests/distribution.rs
@@ -298,127 +298,132 @@ fn two_distributions() {
     assert_eq!(reward_token_2.balance(&user), (reward_amount * 2) as i128);
 }
 
-// #[test]
-// fn four_users_with_different_stakes() {
-//     let env = Env::default();
-//     env.mock_all_auths();
-//
-//     let admin = Address::generate(&env);
-//     let user = Address::generate(&env);
-//     let user2 = Address::generate(&env);
-//     let user3 = Address::generate(&env);
-//     let user4 = Address::generate(&env);
-//     let manager = Address::generate(&env);
-//     let owner = Address::generate(&env);
-//
-//     let lp_token = deploy_token_contract(&env, &admin);
-//     let reward_token = deploy_token_contract(&env, &admin);
-//
-//     let staking = deploy_staking_contract(
-//         &env,
-//         admin.clone(),
-//         &lp_token.address,
-//         &manager,
-//         &owner,
-//         &50u32,
-//     );
-//
-//     staking.create_distribution_flow(&manager, &reward_token.address);
-//
-//     let reward_amount: u128 = 100_000;
-//     reward_token.mint(&admin, &(reward_amount as i128));
-//
-//     // bond tokens for users; each user has a different amount staked
-//     env.ledger().with_mut(|li| {
-//         li.timestamp = ONE_WEEK;
-//     });
-//
-//     lp_token.mint(&user, &1000);
-//     staking.bond(&user, &1000);
-//     lp_token.mint(&user2, &2000);
-//     staking.bond(&user2, &2000);
-//     lp_token.mint(&user3, &3000);
-//     staking.bond(&user3, &3000);
-//     lp_token.mint(&user4, &4000);
-//     staking.bond(&user4, &4000);
-//
-//     let eight_days = ONE_WEEK + ONE_DAY;
-//     env.ledger().with_mut(|li| li.timestamp = eight_days);
-//
-//     let reward_duration = 600;
-//     staking.fund_distribution(
-//         &eight_days,
-//         &reward_duration,
-//         &reward_token.address,
-//         &(reward_amount as i128),
-//     );
-//
-//     env.ledger().with_mut(|li| {
-//         li.timestamp = eight_days + 600;
-//     });
-//     staking.distribute_rewards();
-//
-//     // total staked amount is 10_000
-//     // user1 should have 10% of the rewards, user2 20%, user3 30%, user4 40%
-//     assert_eq!(
-//         staking.query_withdrawable_rewards(&user),
-//         WithdrawableRewardsResponse {
-//             rewards: vec![
-//                 &env,
-//                 WithdrawableReward {
-//                     reward_address: reward_token.address.clone(),
-//                     reward_amount: 10_000
-//                 }
-//             ]
-//         }
-//     );
-//     assert_eq!(
-//         staking.query_withdrawable_rewards(&user2),
-//         WithdrawableRewardsResponse {
-//             rewards: vec![
-//                 &env,
-//                 WithdrawableReward {
-//                     reward_address: reward_token.address.clone(),
-//                     reward_amount: 20_000
-//                 }
-//             ]
-//         }
-//     );
-//     assert_eq!(
-//         staking.query_withdrawable_rewards(&user3),
-//         WithdrawableRewardsResponse {
-//             rewards: vec![
-//                 &env,
-//                 WithdrawableReward {
-//                     reward_address: reward_token.address.clone(),
-//                     reward_amount: 30_000
-//                 }
-//             ]
-//         }
-//     );
-//     assert_eq!(
-//         staking.query_withdrawable_rewards(&user4),
-//         WithdrawableRewardsResponse {
-//             rewards: vec![
-//                 &env,
-//                 WithdrawableReward {
-//                     reward_address: reward_token.address.clone(),
-//                     reward_amount: 40_000
-//                 }
-//             ]
-//         }
-//     );
-//
-//     staking.withdraw_rewards(&user);
-//     assert_eq!(reward_token.balance(&user), 10_000);
-//     staking.withdraw_rewards(&user2);
-//     assert_eq!(reward_token.balance(&user2), 20_000);
-//     staking.withdraw_rewards(&user3);
-//     assert_eq!(reward_token.balance(&user3), 30_000);
-//     staking.withdraw_rewards(&user4);
-//     assert_eq!(reward_token.balance(&user4), 40_000);
-// }
-//
+#[test]
+fn four_users_with_different_stakes() {
+    let env = Env::default();
+    env.mock_all_auths();
+    env.budget().reset_unlimited();
+
+    let admin = Address::generate(&env);
+    let user = Address::generate(&env);
+    let user2 = Address::generate(&env);
+    let user3 = Address::generate(&env);
+    let user4 = Address::generate(&env);
+    let manager = Address::generate(&env);
+
+    let lp_token = deploy_token_contract(&env, &admin);
+    let reward_token = deploy_token_contract(&env, &admin);
+
+    let staking = deploy_staking_contract(
+        &env,
+        admin.clone(),
+        &lp_token.address,
+        &manager,
+        &admin,
+        &50u32,
+    );
+
+    staking.create_distribution_flow(
+        &admin,
+        &reward_token.address,
+        &BytesN::from_array(&env, &[1; 32]),
+        &10,
+        &100,
+        &1,
+    );
+
+    let reward_amount: u128 = 100_000;
+    reward_token.mint(&admin, &(reward_amount as i128));
+
+    // bond tokens for users; each user has a different amount staked
+    lp_token.mint(&user, &1000);
+    staking.bond(&user, &1000);
+    lp_token.mint(&user2, &2000);
+    staking.bond(&user2, &2000);
+    lp_token.mint(&user3, &3000);
+    staking.bond(&user3, &3000);
+    lp_token.mint(&user4, &4000);
+    staking.bond(&user4, &4000);
+
+    // simulate moving forward 60 days for the full APR multiplier
+    env.ledger().with_mut(|li| {
+        li.timestamp = SIXTY_DAYS;
+    });
+
+    let reward_duration = 600;
+    staking.fund_distribution(
+        &SIXTY_DAYS,
+        &reward_duration,
+        &reward_token.address,
+        &(reward_amount as i128),
+    );
+
+    env.ledger().with_mut(|li| {
+        li.timestamp += 600;
+    });
+    staking.distribute_rewards();
+
+    // total staked amount is 10_000
+    // user1 should have 10% of the rewards, user2 20%, user3 30%, user4 40%
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 10_000
+                }
+            ]
+        }
+    );
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user2),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 20_000
+                }
+            ]
+        }
+    );
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user3),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 30_000
+                }
+            ]
+        }
+    );
+    assert_eq!(
+        staking.query_withdrawable_rewards(&user4),
+        WithdrawableRewardsResponse {
+            rewards: vec![
+                &env,
+                WithdrawableReward {
+                    reward_address: reward_token.address.clone(),
+                    reward_amount: 40_000
+                }
+            ]
+        }
+    );
+
+    staking.withdraw_rewards(&user);
+    assert_eq!(reward_token.balance(&user), 10_000);
+    staking.withdraw_rewards(&user2);
+    assert_eq!(reward_token.balance(&user2), 20_000);
+    staking.withdraw_rewards(&user3);
+    assert_eq!(reward_token.balance(&user3), 30_000);
+    staking.withdraw_rewards(&user4);
+    assert_eq!(reward_token.balance(&user4), 40_000);
+}
+
 // #[test]
 // fn two_users_one_starts_after_distribution_begins() {
 //     let env = Env::default();
