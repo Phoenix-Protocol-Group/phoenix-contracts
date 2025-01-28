1915ddb60169cf2dfc193c5b8b5267df0d8f9e1e
gangov <6922910+gangov@users.noreply.github.com>
2025-01-28 14:36:45 +0200

    [no ci] changes the numbers

--- contracts/multihop/src/error.rs
+++ contracts/multihop/src/error.rs
@@ -4,8 +4,8 @@ use soroban_sdk::contracterror;
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
 pub enum ContractError {
-    AlreadyInitialized = 1,
-    OperationsEmpty = 2,
-    IncorrectAssetSwap = 3,
-    AdminNotSet = 4,
+    AlreadyInitialized = 100,
+    OperationsEmpty = 101,
+    IncorrectAssetSwap = 102,
+    AdminNotSet = 103,
 }
