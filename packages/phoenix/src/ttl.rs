9236bfa4ca0be0bab8a0dffa2efbf7aacf121218
gangov <6922910+gangov@users.noreply.github.com>
2025-02-05 18:32:36 +0200

    adds comments to the TTL

--- packages/phoenix/src/ttl.rs
+++ packages/phoenix/src/ttl.rs
@@ -1,12 +1,13 @@
 // Constants for storage bump amounts
 pub const DAY_IN_LEDGERS: u32 = 17280;
 
-// old INSTANCE_BUMP_AMOUNT
+// target TTL for the contract instance and its code.
+// When a TTL extension is triggered the instance's TTL is reset to this value (7 days of ledger units).
 pub const INSTANCE_TARGET_TTL: u32 = 7 * DAY_IN_LEDGERS;
-// old INSTANCE_LIFETIME_THRESHOLD
+// if the current instance TTL falls below this threshold (i.e., less than 6 days of ledger units), the TTL extension mechanism will refresh it to INSTANCE_TARGET_TTL.
 pub const INSTANCE_RENEWAL_THRESHOLD: u32 = INSTANCE_TARGET_TTL - DAY_IN_LEDGERS;
 
-// old PERSISTENT_BUMP_AMOUNT
+// when TTL extension (if the current TTL is below its renewal threshold), the persistent TTL is set to this value (30 days of ledger units).
 pub const PERSISTENT_TARGET_TTL: u32 = 30 * DAY_IN_LEDGERS;
-// old PERSISTENT_LIFETIME_THRESHOLD
+// if the current persistent TTL drops below this threshold (i.e., less than 29 days of ledger units), the TTL extension will bump it back to PERSISTENT_TARGET_TTL.
 pub const PERSISTENT_RENEWAL_THRESHOLD: u32 = PERSISTENT_TARGET_TTL - DAY_IN_LEDGERS;
