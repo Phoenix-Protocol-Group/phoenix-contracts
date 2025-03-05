c62fabbdb1edf2ad9a228b6416417b3d3f02b901
gangov <6922910+gangov@users.noreply.github.com>
2025-03-05 15:29:43 +0200

    basic sanity check works

--- contracts/pool/src/certora/spec.rs
+++ contracts/pool/src/certora/spec.rs
@@ -1,13 +1,7 @@
-use soroban_sdk::Env;
-
-use crate::contract::LiquidityPool;
-use crate::contract::LiquidityPoolTrait;
-
 use cvlr::asserts::cvlr_satisfy;
 use cvlr_soroban_derive::rule;
 
 #[rule]
-fn sanity(e: Env) {
-    //let _ = LiquidityPool::query_pool_info(e.clone());
+fn sanity() {
     cvlr_satisfy!(true);
 }
