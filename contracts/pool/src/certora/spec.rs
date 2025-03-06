669ef87c34141dcbacf90db180e6cd7cfb8fdf29
gangov <6922910+gangov@users.noreply.github.com>
2025-03-06 11:55:54 +0200

    certora query for config

--- contracts/pool/src/certora/spec.rs
+++ contracts/pool/src/certora/spec.rs
@@ -1,7 +1,18 @@
+use crate::storage::CONFIG;
 use cvlr::asserts::cvlr_satisfy;
 use cvlr_soroban_derive::rule;
+use soroban_sdk::Env;
+
+use crate::contract::{LiquidityPool, LiquidityPoolTrait};
 
 #[rule]
 fn sanity() {
     cvlr_satisfy!(true);
+}
+
+#[rule]
+fn certora_query_config(env: Env) {
+    certora::require!(env.storage().persistent().has(&CONFIG), "config exists");
+    let _config = LiquidityPool::query_config(env);
+    certora::satisfy!(true);
 }
