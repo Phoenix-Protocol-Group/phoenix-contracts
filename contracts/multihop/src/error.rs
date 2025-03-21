7c414705b1be15ad48fe7c23550bb77e8b3d366e
gangov <6922910+gangov@users.noreply.github.com>
2025-03-21 21:22:52 +0200

    multihop: change nums

--- contracts/multihop/src/error.rs
+++ contracts/multihop/src/error.rs
@@ -4,11 +4,11 @@ use soroban_sdk::contracterror;
 #[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 #[repr(u32)]
 pub enum ContractError {
-    AlreadyInitialized = 100,
-    OperationsEmpty = 101,
-    IncorrectAssetSwap = 102,
-    AdminNotSet = 103,
-    SameAdmin = 104,
-    NoAdminChangeInPlace = 105,
-    AdminChangeExpired = 106,
+    AlreadyInitialized = 200,
+    OperationsEmpty = 201,
+    IncorrectAssetSwap = 202,
+    AdminNotSet = 203,
+    SameAdmin = 204,
+    NoAdminChangeInPlace = 205,
+    AdminChangeExpired = 206,
 }
