cd538cc7c8af645fa23044b40c5725d546d14719
gangov <6922910+gangov@users.noreply.github.com>
2025-04-15 14:07:32 +0300

    vesting: upgrade

--- contracts/vesting/src/tests/setup.rs
+++ contracts/vesting/src/tests/setup.rs
@@ -1,10 +1,10 @@
-use soroban_sdk::{Address, BytesN, Env};
+use soroban_sdk::{testutils::Address as _, Address, BytesN, Env};
 
 use crate::token_contract;
 
 #[allow(clippy::too_many_arguments)]
 pub mod old_vesting {
-    soroban_sdk::contractimport!(file = "../../.artifacts_sdk_update/old_phoenix_vesting.wasm");
+    soroban_sdk::contractimport!(file = "../../.wasm_binaries_mainnet/live_vesting.wasm");
 }
 
 pub fn install_latest_vesting(env: &Env) -> BytesN<32> {
@@ -20,4 +20,67 @@ pub fn deploy_token_contract<'a>(env: &Env, admin: &Address) -> token_contract::
         &env.register_stellar_asset_contract_v2(admin.clone())
             .address(),
     )
+}
+
+#[test]
+#[allow(deprecated)]
+fn upgrade_vesting_contract() {
+    use soroban_sdk::{vec, String};
+
+    use crate::tests::setup::deploy_token_contract;
+
+    let env = Env::default();
+    env.mock_all_auths();
+    env.cost_estimate().budget().reset_unlimited();
+    let admin = Address::generate(&env);
+    let user = Address::generate(&env);
+
+    let token_client = deploy_token_contract(&env, &admin);
+    token_client.mint(&user, &1_000);
+
+    let env = Env::default();
+    env.mock_all_auths();
+    env.cost_estimate().budget().reset_unlimited();
+
+    let admin = Address::generate(&env);
+    let vester1 = Address::generate(&env);
+    let token_client = deploy_token_contract(&env, &admin);
+
+    token_client.mint(&admin, &320);
+
+    let vesting_token_info = old_vesting::VestingTokenInfo {
+        name: String::from_str(&env, "Phoenix"),
+        symbol: String::from_str(&env, "PHO"),
+        decimals: 6,
+        address: token_client.address.clone(),
+    };
+
+    let vesting_schedules = vec![
+        &env,
+        old_vesting::VestingSchedule {
+            recipient: vester1.clone(),
+            curve: old_vesting::Curve::SaturatingLinear(old_vesting::SaturatingLinear {
+                min_x: 0,
+                min_y: 120,
+                max_x: 60,
+                max_y: 0,
+            }),
+        },
+        old_vesting::VestingSchedule {
+            recipient: Address::generate(&env),
+            curve: old_vesting::Curve::SaturatingLinear(old_vesting::SaturatingLinear {
+                min_x: 15,
+                min_y: 200,
+                max_x: 60,
+                max_y: 0,
+            }),
+        },
+    ];
+    let vesting_addr = env.register_contract_wasm(None, old_vesting::WASM);
+
+    let old_vesting_client = old_vesting::Client::new(&env, &vesting_addr);
+
+    old_vesting_client.initialize(&admin, &vesting_token_info, &10u32);
+
+    old_vesting_client.create_vesting_schedules(&vesting_schedules);
 }
